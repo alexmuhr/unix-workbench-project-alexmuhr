e3c3d548cf264ee19385267abd736578f9f9f1b0
alexmuhr <acmuhr@gmail.com>
2020-07-06 09:36:03 -0700

    updated guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,14 +1,14 @@
 # use ls and egrep to count the number of files in the current directory
-numfiles=$(ls -p | grep -v / | wc -l)
+numfiles=$(ls -A | wc -l)
 iscorrect=0
 
 # function that checks a player's guess against the correct answer
 function checkguess {
-  if [[ $1 -eq $2 ]]
+  if [[ $1 -eq $numfiles ]]
   then
     iscorrect=1
     echo "You guessed correctly!"
-  elif [[ $1 -lt $2 ]]
+  elif [[ $1 -lt $numfiles ]]
   then
     echo "Too low"
   else
@@ -21,5 +21,5 @@ while [[ iscorrect -eq 0 ]]
 do
   echo "Guess the number of files in this directory: "
   read guess
-  checkguess $guess $numfiles
+  checkguess $guess
 done
